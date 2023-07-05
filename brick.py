afc73d70240cd763f0b9a2cb72871a381cf381a6
jriyyya <jriya.shi@gmail.com>
2023-07-05 11:33:36 +0000

    removed speed and position from Brick

--- brick.py
+++ brick.py
@@ -8,14 +8,16 @@ class Brick:
         self.rect = pygame.Rect(
                 position[0], position[1], dimensions[0], dimensions[1]
             )
-
-        self.position = pygame.Vector2(position[0], position[1])
-        self.speed = 12
-
+        
         self.color = (0, 0, 0)
 
     def update(self):
         self.draw()
 
-    def draw(self):
+    def draw(self, offset = (0, 0)):
+        x, y = self.rect.x, self.rect.y
+        self.rect.x += offset [0]
+        self.rect.y += offset [1]
         pygame.draw.rect(self.gameDisplay, self.color, self.rect)
+        self.rect.x = x
+        self.rect.y = y
