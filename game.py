89745ae2f11578ed4118a289688cbd8c6fc42048
Ali Hassan <141296395+AliHassan245@users.noreply.github.com>
2025-04-06 21:11:07 +0500

    Update game.py

--- game.py
+++ game.py
@@ -39,6 +39,7 @@ class BallAndBrick:
         self.back_col = (255, 255, 255)
         self.brick_col = (0, 0, 0)
         self.scroll_col = (0, 0, 0)
+        self.high_score = 0
         self.ball_still = 0
         self.ball_play = 1
         self.ball_won = 2
@@ -297,7 +298,9 @@ class BallAndBrick:
             for brick in self.bricks_arr:
                 if self.ball.colliderect(brick):
                     self.brick_hit_sound.play()
-                    self.score += 3
+                    self.score += 1
+                    if self.score > self.high_score:
+                        self.high_score = self.score
                     self.bricks_arr.remove(brick)
                     self.shake = pygame.time.get_ticks() + 200
                     dr = abs(self.ball.right - brick.left)
@@ -349,11 +352,11 @@ class BallAndBrick:
                 live_surface,
                 (round(screen.get_width() / 2 - live_surface.get_width() / 2), 5),
             )
-            pause_mess = medfont.render("Press P to pause", True, self.brick_col)
-            screen.blit(
-                pause_mess,
-                (round(screen.get_width() - pause_mess.get_width() - sx(100)), 5),
+            high_score_surface = medfont.render(
+               "High Score: " + str(self.high_score), True, self.brick_col
             )
+            screen.blit(high_score_surface, (screen.get_width() - high_score_surface.get_width() - sx(100), 5))
+
 
         def text_size(text, color, size):
             screen = pygame.display.get_surface()
